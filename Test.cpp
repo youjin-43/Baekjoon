48eaedcf896a7e4df2d648f60e184a486ac77376
youjin43 <wlwlfdl3324@naver.com>
2024-03-12 23:30:49 +0900

    5597번

--- Test.cpp
+++ Test.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 int main(){
-    int a,b;
-    while(    cin>>a>>b){
-            
-    cout<<a+b<<endl;
+    int list_[31]={0,};
+
+    int n;
+    for(int i=0;i<28;i++){
+        cin>>n;
+        list_[n]++;
     }
 
-
+    for(int i=1;i<=30;i++){
+        if(list_[i] != 1){
+            // cout<<"hit"<<endl;
+            cout<<i<<endl;
+        }
+    }
 }
