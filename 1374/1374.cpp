0252b731a5a62a20c7c1ecf5e65482db2772e97d
youjin43 <wlwlfdl3324@naver.com>
2024-11-13 13:53:30 +0900

    1374_강의실

--- 1374/1374.cpp
+++ 1374/1374.cpp
@@ -1,40 +1,40 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include<queue>
 using namespace std;
 
-bool cmp(pair<int,int> a,pair<int,int> b);
 
+//모든 강의의 시작하는 시간과 끝나는 시간을 알고 있다. 이때, 우리는 최대한 적은 수의 강의실을 사용하여 모든 강의가 이루어지게 하고 싶다.
+//종료시간과 시작시간이 겹쳐도됨 
+//필요한 최소 강의실의 수를 출력
+
+
+//ret를 갱신할때마다 ret 벡터가 오름차순으로 보장되지않음!! 
+//그냥 start 기준으로 정렬하고, 가장 빠르게 끝나는 시간을 pq로 가지고 있어야겠다.. 
+
+vector<pair<int,int> > list; // 시작시간, 끝나는 시간 
 int main(){
-    int n; //1 ≤ N ≤ 100,000
-    cin>>n;
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    // freopen("input.txt","r",stdin);
 
-    vector <pair<int,int>> time; // 시작시간, 종료시간
-    int a,b;
-    for(int i=0;i<n;i++){
-        cin>>a>>b;
-        time.push_back(make_pair(b,a));
+    int n; cin>>n; //N(1 ≤ N ≤ 100,000)
+    //강의 번호, 강의 시작 시간, 강의 종료 시간(종료 시간은 0 이상 10억 이하)
+    for(int i=0,tmp,start,end;i<n;i++){
+        cin>>tmp>>start>>end;
+        list.push_back(make_pair(start,end));
     }
-    sort(time.begin(),time.end(),cmp);
+    sort(list.begin(),list.end()); //시작 시간 기준 소팅
 
-
-    int start = time[0].first;
-    int end = time[0].second;
-    int max =1;
-
+    priority_queue<int,vector<int>,greater<int> > pq; //가장 빨리 끝나는 시간을 top에  
+    pq.push(list[0].second);
     for(int i=1;i<n;i++){
-        
+        if(pq.top()<=list[i].first){
+            pq.pop();
+            
+        }
+        pq.push(list[i].second);
+
     }
-
-
-
-    
-}
-
-// 오름차순 정렬
-bool cmp(pair<int,int> a,pair<int,int> b){
-    if(a.first == b.first){
-        return a.second < b.second;
-    }
-    return a.first < b.first; 
+    cout<<pq.size()<<'\n';
 }
