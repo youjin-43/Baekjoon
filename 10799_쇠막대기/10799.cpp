fd80d1f3a8f8c6dfccf578a571ec54af318c4c38
youjin43 <wlwlfdl3324@naver.com>
2025-04-02 00:46:46 +0900

    10799_쇠막대기

--- 10799_쇠막대기/10799.cpp
+++ 10799_쇠막대기/10799.cpp
@@ -1,37 +1,25 @@
-    //25.03.31(일) 10799 쇠막대기
-    #include<iostream>
-    #include<stack>
-    using namespace std;
-    int main(){
-        ios::sync_with_stdio(0);cin.tie(0);
-        
-        char list[100001]; cin>>list;
-    
-        stack<char> st;
-        st.push(list[0]);
-        
-        bool isLaser=0;
-        int ret=0;
-    
-        int i=1;
-        while(list[i] !='\0'){
-             // '(' 인 경우 
-            if(list[i] =='('){
-                st.push('(');
-                isLaser=0;
-            }
-            // ')' 인 경우 
-            else{
-                st.pop();
-    
-                if(isLaser){ // 이전이 ')'이면 막대 끝!
-                    ret++;
-                } else { // 이전이 '('이면 레이저!
-                    isLaser=1;
-                    ret+=st.size(); // 현재 열려있는 막대 수만큼 조각
-                }
-            }
-            i++;
+#include<cstdio>
+
+int a; // 잘린 조각 수 (출력할 값)
+int n; // 현재 열려있는 ( 괄호 수 → 현재 열려있는 막대기 수
+
+int main() {
+    char c[100001], b='.';
+    scanf("%s", c);
+
+    for (int i; c[i]; i++) {
+    	// '(' 인 경우 
+        if (c[i] == '(') {
+            n++; // 열린 괄호면 막대기 시작, 열려있는 막대 수 증가
+        } 
+        // ')' 인 경우 
+        else {
+            n--; // 닫힌 괄호면 막대기 하나 종료됨
+            if (b == ')') a++; // 이전도 닫힌 괄호면 막대기 끝 → 조각 +1
+            else a += n;  // 레이저면 현재 열려있는 막대기 수만큼 조각 추가
         }
-        cout<<ret<<'\n';
+        b = c[i]; // 현재 문자를 이전 문자로 저장
     }
+
+    printf("%d", a);
+}
