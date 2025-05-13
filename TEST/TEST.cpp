32375adecb88926a7ac6467bfe7b600d28754ac8
youjin43 <wlwlfdl3324@naver.com>
2025-05-13 23:28:06 +0900

    Update TEST.cpp

--- TEST/TEST.cpp
+++ TEST/TEST.cpp
@@ -1,8 +1,18 @@
 #include<iostream>
 using namespace std;
 int main() {
-    int n,m; cin>>n>>m;
-    if(m==1 || m==2) cout<<"NEWBIE!";
-    else if(m<=n) cout<<"OLDBIE!";
-    else cout<<"TLE!";
+    int t; cin>>t;
+    while(t--) {
+        int a,b,c; cin>>a>>b>>c;
+
+        if(b-c>=a){cout<<0<<endl;}
+
+        else if(a==1) cout<<(c-b)<<endl;
+        else if(a==2){
+            cout<<(c-b)*3<<endl;
+      
+        }
+        else{cout<<(c-b)*5<<endl;}
+        
+    }
 }
